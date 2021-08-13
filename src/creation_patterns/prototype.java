0c9c9e214cd4a55c05790d8eb232f74e95324395
RowanEklund <717538@dpsk12.net>
2021-08-13 00:36:50 -0600

    Added Singleton Design Pattern

--- src/creation_patterns/prototype.java
+++ src/creation_patterns/prototype.java
@@ -3,24 +3,24 @@ package creation_patterns;
 import java.util.ArrayList;
 import java.util.List;
 
-abstract class common_abs {
+abstract class commonAbs {
 //    abstract class for all objects to conform to
     public int num;
     public String str;
 
-    public common_abs () {}
+    public commonAbs () {}
 
-    public common_abs(common_abs proto) {
+    public commonAbs(commonAbs proto) {
 //        if original has been instantiated then set internal variables equal to original's
         if (proto != null) {
             this.num = proto.num;
             this.str = proto.str;
         }
     }
-    public abstract common_abs clone();
+    public abstract commonAbs clone();
 }
 
-class var1 extends common_abs {
+class var1 extends commonAbs {
 //    creates an object which can be cloned
     public int num;
     
@@ -37,7 +37,7 @@ class var1 extends common_abs {
     }
 }
 
-class var2 extends common_abs {
+class var2 extends commonAbs {
     //    creates a different object which can be cloned
     public int num;
     public String str;
@@ -61,22 +61,22 @@ public class prototype {
 //    objects supporting this copying or "cloning" are prototypes
 //    can be used when combined with factories to create many prototypes quickly
     public void run() {
-        List<common_abs> regulars = new ArrayList<>();
-        List<common_abs> clones = new ArrayList<>();
+        List<commonAbs> regulars = new ArrayList<>();
+        List<commonAbs> clones = new ArrayList<>();
 
-//        Create an object to act as the prototype
+//        create an object to act as the prototype
         var1 v1 = new var1();
         v1.num = 254;
         regulars.add(v1);
-//        Create clone of prototype
+//        create clone of prototype
         var1 v1clone = v1.clone();
         clones.add(v1clone);
 
-//        Create a different object to act as the prototype
+//        create a different object to act as the prototype
         var2 v2 = new var2();
         v2.num = 254;
         regulars.add(v2);
-//        Create clone of prototype
+//        create clone of prototype
         var2 v2clone = v2.clone();
         clones.add(v2clone);
 
--- src/creation_patterns/singleton.java
+++ src/creation_patterns/singleton.java
@@ -1,4 +1,84 @@
 package creation_patterns;
 
+final class singletonSingleThread {
+//    declared final in scope in order to prevent duplicate creation
+    private static singletonSingleThread instance;
+    public String str;
+
+    private singletonSingleThread(String str) {
+//        emulates slow initialization to make sure that two objects were not created at the same time
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.str = str;
+    }
+//    making sure to only create a singleton if one is not currently instantiated
+    public static singletonSingleThread getInstance(String str) {
+        if (instance == null) {
+            instance = new singletonSingleThread(str);
+        }
+        return instance;
+    }
+}
+
+final class singletonMultiThread {
+    //    declared final in scope in order to prevent duplicate creation
+    private static singletonMultiThread instance;
+    public String str;
+
+    private singletonMultiThread(String str) {
+//        emulates slow initialization to make sure that two objects were not created at the same time
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.str = str;
+    }
+    //    making sure to only create a singleton if one is not currently instantiated
+    public static singletonMultiThread getInstance(String str) {
+        if (instance == null) {
+            instance = new singletonMultiThread(str);
+        }
+        return instance;
+    }
+}
+
 public class singleton {
+//    created to ensure that a class has only one instance that is globally accessible
+//    creates a private default constructor to prevent multiple objects being created
+//    allows for easy usage limitation and caping
+
+//    creates two different objects, one for each thread
+    static class Thread1 implements Runnable {
+        @Override
+    public void run() {
+//            print should return val1 or val2, but not both
+            singletonMultiThread sm = singletonMultiThread.getInstance("val1 multi");
+            System.out.println(sm.str);
+        }
+    }
+
+    static class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            singletonMultiThread sm = singletonMultiThread.getInstance("val2 multi");
+            System.out.println(sm.str);
+        }
+    }
+
+    public void run() {
+        singletonSingleThread s1s = singletonSingleThread.getInstance("val1 single");
+        singletonSingleThread s2s = singletonSingleThread.getInstance("val2 single");
+//        print should return either two val1 or two val2, as only one singleton can exist at a time
+        System.out.println(s1s + " " + s2s);
+
+//        creates two different threads for the multithreaded singleton
+        Thread thread1 = new Thread(new Thread1());
+        Thread thread2 = new Thread(new Thread2());
+        thread1.start();
+        thread2.start();
+    }
 }
